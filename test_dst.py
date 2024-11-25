b7cfe613e781123436126335d4a12f7e9e4a6b31
René Hernández <rhernandez@sistemasesco.com>
2024-11-25 01:47:10 -0300

    test: Add initial test suite for destination functionality

---
+++ test_dst.py
@@ -0,0 +1,14 @@
+import arrow
+
+def test_dst():
+    # 1667725199 11/6/2022 1:59:59AM GMT-7 DST
+    # 1667725200 11/6/2022 1:00:00AM GMT-8
+    before_dst = arrow.get(1667725199, tzinfo='America/Los_Angeles')
+    print(before_dst)
+    after_dst = arrow.get(1667725200, tzinfo='America/Los_Angeles')
+    print(after_dst)
+    secs = (after_dst - before_dst).total_seconds()
+    print(secs)
+
+
+test_dst()
